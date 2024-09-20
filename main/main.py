41936b6713e190d8ac1686590a48d7f13c1a838f
Urbnauq <eduardoalmonte18@gmail.com>
2024-09-20 14:44:15 -0400

    can rotate player

--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     
@@ -12,17 +13,22 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
     while True:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
-        #limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000
+        #limit the framerate to 144 FPS
+        dt = clock.tick(144) / 1000
     
 if __name__ == "__main__":
     main()
